7da3d5f4167d6b4231fbafd4cd3d3ce72deabc02
taeeun kim <kimtaeeeny@gmail.com>
2020-05-09 01:58:13 +0900

    Refactor : 컨테이너 파일명, 변수명 수정

---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 60px;
+  background: red;
+`;
+
+const HeaderContainer = () => {
+  return <Container></Container>;
+};
+
+export default HeaderContainer;
---
+++ src/containers/LayerContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: orange;
+  flex: 2;
+`;
+
+const LayerContainer = () => {
+  return <Container></Container>;
+};
+
+export default LayerContainer;

93859ca9721b18cedfd9b0157bcccb02a6551fed
samslow <shsvkdldj@likelion.org>
2020-05-10 02:12:48 +0900

    Feature: Toolbox Container 생성

---
+++ src/containers/ToolboxContainer.js
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import { AiOutlineRotateRight } from "react-icons/ai";
+import { MdMessage } from "react-icons/md";
+import { FaShapes } from "react-icons/fa";
+import { BsFillImageFill } from "react-icons/bs";
+import { IoIosColorFilter } from "react-icons/io";
+
+import ToolboxContentBox from "components/Toolbox/ContentBox";
+
+const Toolbox = () => {
+  const handleTool = (name) => {
+    alert(name);
+  };
+
+  return (
+    <Container>
+      <ContentRow>
+        <ToolboxContentBox
+          name={"화면 회전"}
+          icon={<AiOutlineRotateRight size="60%" color={"#888"} />}
+          onClickTool={(name) => handleTool(name)}
+        />
+        <ToolboxContentBox
+          name={"텍스트 삽입"}
+          icon={<MdMessage size="60%" color={"#888"} />}
+          onClickTool={(name) => handleTool(name)}
+        />
+        <ToolboxContentBox
+          name={"도형 삽입"}
+          icon={<FaShapes size="60%" color={"#888"} />}
+          onClickTool={(name) => handleTool(name)}
+        />
+      </ContentRow>
+      <ContentRow>
+        <ToolboxContentBox
+          name={"이미지 삽입"}
+          icon={<BsFillImageFill size="60%" color={"#888"} />}
+          onClickTool={(name) => handleTool(name)}
+        />
+        <ToolboxContentBox
+          name={"필터"}
+          icon={<IoIosColorFilter size="60%" color={"#888"} />}
+          onClickTool={(name) => handleTool(name)}
+        />
+        <ToolboxContentBox />
+      </ContentRow>
+      <ContentRow>
+        <ToolboxContentBox />
+        <ToolboxContentBox />
+        <ToolboxContentBox />
+      </ContentRow>
+    </Container>
+  );
+};
+
+const Container = styled.aside`
+  display: flex;
+  flex-direction: column;
+  background: #eee;
+`;
+
+const ContentRow = styled.div`
+  display: flex;
+  width: "100%";
+  height: 100px;
+`;
+
+export default Toolbox;

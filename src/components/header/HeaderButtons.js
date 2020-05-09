22c57e8aa33593d50dc72d9dc2baa8848249adf0
taeeun kim <kimtaeeeny@gmail.com>
2020-05-10 00:20:54 +0900

    Refactor : Header 컨테이너에서 컴포넌트 분리

---
+++ src/components/header/HeaderButtons.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useCallback, useState } from "react";
+import styled from "styled-components";
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  right: 0;
+  display: flex;
+  padding: 0px 30px 0px 30px;
+
+  & > button {
+    cursor: pointer;
+    width: 70px;
+    height: 30px;
+    font-size: 12px;
+    margin: 0px 5px 0px 5px;
+    border-radius: 3px;
+  }
+`;
+
+const VERTICAL_SHAPE = "VERTICAL_SHAPE";
+const HORIZONTAL_SHAPE = "HORIZONTAL_SHAPE";
+
+const HeaderButtons = ({ verticalText, horizontalText, downloadText }) => {
+  const [nowShape, setNowShape] = useState(horizontalText);
+
+  useEffect(() => {
+    console.log(nowShape);
+  }, [nowShape]);
+
+  const changeShape = useCallback((shapeType) => {
+    if (shapeType === VERTICAL_SHAPE) {
+      return setNowShape(VERTICAL_SHAPE);
+    }
+    if (shapeType === HORIZONTAL_SHAPE) {
+      return setNowShape(HORIZONTAL_SHAPE);
+    }
+  }, []);
+  return (
+    <ButtonContainer>
+      <button onClick={() => changeShape(VERTICAL_SHAPE)}>
+        {verticalText}
+      </button>
+      <button onClick={() => changeShape(HORIZONTAL_SHAPE)}>
+        {horizontalText}
+      </button>
+      <button>{downloadText}</button>
+    </ButtonContainer>
+  );
+};
+
+export default HeaderButtons;
--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,5 +1,7 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
+import Title from "../components/header/Title";
+import HeaderButtons from "../components/header/HeaderButtons";
 
 const Container = styled.div`
   position: relative;
@@ -10,65 +12,20 @@ const Container = styled.div`
   background: #2db400;
 `;
 
-const Title = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-  color: white;
-  padding: 0px 30px 0px 30px;
-`;
-
-const ButtonContainer = styled.div`
-  position: absolute;
-  right: 0;
-  display: flex;
-  padding: 0px 30px 0px 30px;
-
-  & > button {
-    cursor: pointer;
-    width: 70px;
-    height: 30px;
-    font-size: 12px;
-    margin: 0px 5px 0px 5px;
-    border-radius: 3px;
-  }
-`;
-
 const APP_TITLE_TEXT = "마중그림";
 const VERTICAL_TEXT = "세로형";
 const HORIZONTAL_TEXT = "가로형";
 const DOWNLOAD_TEXT = "다운로드";
 
-const VERTICAL_SHAPE = "VERTICAL_SHAPE";
-const HORIZONTAL_SHAPE = "HORIZONTAL_SHAPE";
-
 const HeaderContainer = () => {
-  const [nowShape, setNowShape] = useState(HORIZONTAL_SHAPE);
-
-  useEffect(() => {
-    console.log(nowShape);
-  }, [nowShape]);
-
-  const changeShape = useCallback((shapeType) => {
-    if (shapeType === VERTICAL_SHAPE) {
-      return setNowShape(VERTICAL_SHAPE);
-    }
-    if (shapeType === HORIZONTAL_SHAPE) {
-      return setNowShape(HORIZONTAL_SHAPE);
-    }
-  }, []);
-
   return (
     <Container>
-      <Title>{APP_TITLE_TEXT}</Title>
-      <ButtonContainer>
-        <button onClick={() => changeShape(VERTICAL_SHAPE)}>
-          {VERTICAL_TEXT}
-        </button>
-        <button onClick={() => changeShape(HORIZONTAL_SHAPE)}>
-          {HORIZONTAL_TEXT}
-        </button>
-        <button>{DOWNLOAD_TEXT}</button>
-      </ButtonContainer>
+      <Title appTitleText={APP_TITLE_TEXT} />
+      <HeaderButtons
+        verticalText={VERTICAL_TEXT}
+        horizontalText={HORIZONTAL_TEXT}
+        downloadText={DOWNLOAD_TEXT}
+      />
     </Container>
   );
 };

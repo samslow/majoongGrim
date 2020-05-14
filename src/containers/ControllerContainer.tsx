251b90048da39f4e80c19ebb09e2bc1e0979a8d1
samslow <shsvkdldj@likelion.org>
2020-05-15 05:25:29 +0900

    Feature: Controller Switching 기능 추가

--- src/containers/ControllerContainer.tsx
+++ src/containers/ControllerContainer.tsx
@@ -3,22 +3,40 @@ import styled from "styled-components";
 import { observer } from "mobx-react";
 
 import useStores from "hooks/useStores";
+import { ToolboxType } from "containers/ToolboxContainer";
+import InsertText from "components/controllers/InsertText";
+import DefaultController from "components/controllers/DefaultController";
 
 const ControllerContainer = observer(() => {
   const { ToolboxStore } = useStores();
 
+  const SwitchController = ({ type }: { type: string }) => {
+    let result;
+
+    switch (type) {
+      case ToolboxType.TEXT:
+        result = <InsertText />;
+        break;
+      case ToolboxType.IMAGE:
+      default:
+        result = <DefaultController />;
+        break;
+    }
+    return result;
+  };
+
   return (
     <Container>
-      {ToolboxStore.selectedTool
-        ? ToolboxStore.selectedTool
-        : "툴을 선택하세요."}
+      <SwitchController type={ToolboxStore.selectedTool} />
     </Container>
   );
 });
 
 const Container = styled.div`
-  background: purple;
+  background: #555;
   flex: 1;
+  display: flex;
+  padding: 2em;
 `;
 
 export default ControllerContainer;

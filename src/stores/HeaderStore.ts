4c07b12b247de89c74e0d8608589f107a0796591
taeeun kim <kimtaeeeny@gmail.com>
2020-05-14 22:11:50 +0900

    Refactor : mobX 안쓰는 기능 (action) 제거

--- src/stores/HeaderStore.ts
+++ src/stores/HeaderStore.ts
@@ -1,4 +1,4 @@
-import { observable, action } from "mobx";
+import { observable } from "mobx";
 
 export default class HeaderStore {
   @observable nowShape = "HORIZONTAL_SHAPE";
--- src/stores/ToolboxStore.ts
+++ src/stores/ToolboxStore.ts
@@ -1,4 +1,4 @@
-import { observable, action } from "mobx";
+import { observable } from "mobx";
 
 export default class ToolboxStore {
   @observable selectedTool = null;

3c5d367a5e27eea90249027f62eb629a7da6e718
taeeun kim <kimtaeeeny@gmail.com>
2020-05-14 19:05:38 +0900

    Fix : resizeImage 함수모듈 조건문 수정

--- src/modules/resizeImage.ts
+++ src/modules/resizeImage.ts
@@ -1,6 +1,8 @@
 export const resizeImage = (image: HTMLImageElement, nowShape: string) => {
   let MAX_WIDTH: number;
   let MAX_HEIGHT: number;
+  let width = image.width;
+  let height = image.height;
 
   if (nowShape === "HORIZONTAL_SHAPE") {
     MAX_WIDTH = 480;
@@ -12,20 +14,23 @@ export const resizeImage = (image: HTMLImageElement, nowShape: string) => {
     MAX_WIDTH = 280;
     MAX_HEIGHT = 280;
   }
-
-  let width = image.width;
-  let height = image.height;
-
   if (width > height) {
     if (width > MAX_WIDTH) {
       height *= MAX_WIDTH / width;
       width = MAX_WIDTH;
+    } else {
+      width *= MAX_HEIGHT / height;
+      height = MAX_HEIGHT;
     }
   } else {
     if (height > MAX_HEIGHT) {
       width *= MAX_HEIGHT / height;
       height = MAX_HEIGHT;
+    } else {
+      height *= MAX_WIDTH / width;
+      width = MAX_WIDTH;
     }
   }
+
   return [width, height];
 };

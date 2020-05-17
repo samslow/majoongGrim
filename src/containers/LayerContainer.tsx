424b0c891d65ad80da129d257ee0d86517bcecb7
samslow <shsvkdldj@likelion.org>
2020-05-17 22:03:03 +0900

    Feature: Layer 컴포넌트 생성

--- src/containers/LayerContainer.tsx
+++ src/containers/LayerContainer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Layer from "components/layers/Layer";
 
 const Container = styled.div`
-  background: orange;
   flex: 2;
 `;
 

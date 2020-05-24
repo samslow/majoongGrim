128eb264e8749f19f16ebb6e93f61ef7a72eb398
taeeun kim <kimtaeeeny@gmail.com>
2020-05-24 21:48:22 +0900

    Fix : 첫번째 드래그시, 고스트이미지 안나오게 수정

--- src/components/editor/DraggableImage.tsx
+++ src/components/editor/DraggableImage.tsx
@@ -23,30 +23,37 @@ const DraggableImage: React.FC<ComponentProps> = ({ layer, onClick }) => {
   // 드래그 시작시, e.client 좌표
   const [firstEventClientX, setFirstEventClientX] = useState(0);
   const [firstEventClientY, setFirstEventClientY] = useState(0);
+  const [
+    transparentImage,
+    setTransparentImage,
+  ] = useState<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src =
+      "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
+    img.onload = () => {
+      setTransparentImage(img);
+    };
+  }, []);
 
   // 드래그 스타트 (기존의 이미지좌표와 e.client좌표 저장)
   const onDragStartImageHandler = useCallback(
     (e: React.DragEvent<HTMLImageElement>) => {
-      const img = new Image();
-      img.src =
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-      e.dataTransfer.setDragImage(img, 0, 0);
+      transparentImage && e.dataTransfer.setDragImage(transparentImage, 0, 0);
       onClick(id, imgX, imgY, width, height, angleDegree, false);
       setFirstEventClientX(e.clientX);
       setFirstEventClientY(e.clientY);
       setFirstImgX(imgX);
       setFirstImgY(imgY);
     },
-    [imgX, imgY],
+    [imgX, imgY, transparentImage],
   );
 
   // 드래그 도중 (기존의 이미지좌표의 e.client좌표와 현재 드래그 도중의 e.client좌표 차이만큼 이미지좌표에 더해서 저장)
   const onDragImageHandler = useCallback(
     (e: React.DragEvent<HTMLImageElement>) => {
-      const img = new Image();
-      img.src =
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-      e.dataTransfer.setDragImage(img, 0, 0);
+      transparentImage && e.dataTransfer.setDragImage(transparentImage, 0, 0);
       const xChange = e.clientX - firstEventClientX;
       const yChange = e.clientY - firstEventClientY;
       setImgX(firstImgX + xChange);

fa41454ad3cff9739aaa3ede29871e57e9ace7fa
taeeun kim <kimtaeeeny@gmail.com>
2020-05-24 23:04:25 +0900

    Refactor : useDrag 훅 생성

--- src/components/editor/DraggableText.tsx
+++ src/components/editor/DraggableText.tsx
@@ -3,6 +3,7 @@ import TextLayer from "modules/layers/TextLayer";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { CHANGE_LAYER_LOCATION } from "store/layerReducer";
+import useDrag from "hooks/useDrag";
 
 interface ComponentProps {
   layer: TextLayer;
@@ -10,79 +11,26 @@ interface ComponentProps {
 }
 
 const DraggableText: React.FC<ComponentProps> = ({ layer, onClick }) => {
-  const dispatch = useDispatch();
+  const {
+    imgX,
+    imgY,
+    onDragStartImageHandler,
+    onDragImageHandler,
+    onDragOverImageHandler,
+    onDragEndImageHandler,
+  } = useDrag(layer, onClick);
   const {
     id,
-    x,
-    y,
     width,
     height,
+    angleDegree,
     zIndex,
     fontFamily,
-    fontType,
     fontSize,
-    color,
+    fontType,
     content,
-    angleDegree,
+    color,
   } = layer;
-  // 텍스트 좌표
-  const [imgX, setImgX] = useState(x);
-  const [imgY, setImgY] = useState(y);
-  // 드래그 시작시, 텍스트 좌표
-  const [firstImgX, setFirstImgX] = useState(0);
-  const [firstImgY, setFirstImgY] = useState(0);
-  // 드래그 시작시, e.client 좌표
-  const [firstEventClientX, setFirstEventClientX] = useState(0);
-  const [firstEventClientY, setFirstEventClientY] = useState(0);
-
-  // 드래그 스타트 (기존의 텍스트좌표와 e.client좌표 저장)
-  const onDragStartImageHandler = useCallback(
-    (e: React.DragEvent<HTMLImageElement>) => {
-      const img = new Image();
-      img.src =
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-      e.dataTransfer.setDragImage(img, 0, 0);
-      onClick(id, imgX, imgY, width, height, angleDegree, false);
-      setFirstEventClientX(e.clientX);
-      setFirstEventClientY(e.clientY);
-      setFirstImgX(imgX);
-      setFirstImgY(imgY);
-    },
-    [imgX, imgY],
-  );
-
-  // 드래그 도중 (기존의 텍스트좌표의 e.client좌표와 현재 드래그 도중의 e.client좌표 차이만큼 텍스트좌표에 더해서 저장)
-  const onDragImageHandler = useCallback(
-    (e: React.DragEvent<HTMLImageElement>) => {
-      const img = new Image();
-      img.src =
-        "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=";
-      e.dataTransfer.setDragImage(img, 0, 0);
-      const xChange = e.clientX - firstEventClientX;
-      const yChange = e.clientY - firstEventClientY;
-      setImgX(firstImgX + xChange);
-      setImgY(firstImgY + yChange);
-    },
-    [firstEventClientX, firstEventClientY, firstImgX, firstImgY],
-  );
-
-  // drop을 사용하려면 dragover 이벤트핸들러를 만들고 e.preventDefault()로 전파를 막아야주어야함.
-  const onDragOverImageHandler = useCallback(
-    (e: React.DragEvent<HTMLImageElement>) => {
-      e.preventDefault();
-    },
-    [],
-  );
-
-  // 드래그 종료 (redux layers 상태변경)
-  const onDragEndImageHandler = useCallback(() => {
-    dispatch({
-      type: CHANGE_LAYER_LOCATION,
-      id: id,
-      x: imgX,
-      y: imgY,
-    });
-  }, [imgX, imgY]);
 
   const onClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
